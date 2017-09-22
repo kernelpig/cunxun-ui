23fd5c48d14e26d652b913e49357de28c955248a
kernelpig <18653193004@163.com>
2017-09-22 16:47:19 +0800

    移动页面适配修改
    1. 页面支持移动；
    2. 列表页移动端只显示标题；

--- src/assets/js/model/list.js
+++ src/assets/js/model/list.js
@@ -30,9 +30,9 @@ function ArticleGetListHandler() {
                 $.each(data['list'], function () {
                     var href = 'article.html?article_id=' + this.id;
                     var link = $('<a class="am-list-item-hd"></a>').attr('href', href).text(this.title);
-                    var author = $('<span class="am-list-author"></span>').text(this.nickname);
-                    var stat = $('<span class="am-list-stat"></span>').text(this.comment_count);
-                    var time = $('<span class="am-list-date"></span>').text(GMT2Beijing(this.updated_at));
+                    var author = $('<span class="am-list-author am-show-lg-up"></span>').text(this.nickname);
+                    var stat = $('<span class="am-list-stat am-show-lg-up"></span>').text(this.comment_count);
+                    var time = $('<span class="am-list-date am-show-lg-up"></span>').text(GMT2Beijing(this.updated_at));
                     $(".am-list").append($('<li class="am-g"></li>').append(link).append(author).append(stat).append(time));
                 });
             }
