a2a2bd803927b2d9586f1c3013c00b9d83d8a4fa
kernelpig <18653193004@163.com>
2017-09-25 01:24:43 +0800

    增加修改时显示文章

--- src/assets/js/page/admin/article.js
+++ src/assets/js/page/admin/article.js
@@ -32,14 +32,14 @@ function ArticleCreateHandler() {
     });
 }
 
-function ArticleCreateRender() {
-    // 加载栏目列表
-    ColumnGetListHandler();
-    $(".ArticleActionHandler").click(ArticleCreateHandler);
+function ArticleUpdateHandler() {
+    // TODO: 调用文章更新接口
 }
 
-$(document).ready(function () {
-    ArticlePageRender();
+function ArticleCreateRender() {
+    $(".ArticleActionHandler").click(ArticleCreateHandler);
+    // 没有找到编辑框的设置文本方法, 使用的较老版本, 暂时使用此方法规避掉
+    // TODO: 更新为最新版本的编辑器
     $(".ArticleContentField").editable({
         inlineMode: false,
         alwaysBlank: true,
@@ -48,10 +48,43 @@ $(document).ready(function () {
         language: 'zh_cn',
         pluginsEnabled: ['fullscreen']
     });
+}
+
+function ArticleUpdateRender() {
+    $(".ArticleActionHandler").click(ArticleUpdateHandler);
+    var req = {
+        "article_id": GetURIParamStr(location.href, "article_id")
+    };
+    APIArticleGetItem(req, AlertShowAjaxError, function (data) {
+        if (data["code"] === 0) {
+            $(".ColumnListField").val(data["item"].column_id);
+            $(".ArticleTitleField").val(data["item"].title);
+            $('.ArticleContentField').val(data["item"].content);
+            // 没有找到编辑框的设置文本方法, 使用的较老版本, 暂时使用此方法规避掉
+            // TODO: 更新为最新版本的编辑器
+            $(".ArticleContentField").editable({
+                inlineMode: false,
+                alwaysBlank: true,
+                theme: 'gray',
+                height: 200,
+                language: 'zh_cn',
+                pluginsEnabled: ['fullscreen']
+            });
+            //$('.ArticleContentField').editable('html', data["item"].content);
+        } else {
+            AlertShowError(data["sub_error"]);
+        }
+    });
+}
+
+$(document).ready(function () {
+    ArticlePageRender();
+    ColumnGetListHandler();
+
     var action = GetURIParamStr(location.href, "action");
     if (action === "create") {
         ArticleCreateRender();
     } else if (action === "update") {
-
+        ArticleUpdateRender();
     }
 });
