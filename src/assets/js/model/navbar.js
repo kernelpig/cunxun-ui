cf51bd74ac8fc7bdb4542afa84406fe255e98cdf
kernelpig <18653193004@163.com>
2017-10-09 23:41:26 +0800

    优化navbar显示方式

--- src/assets/js/model/navbar.js
+++ src/assets/js/model/navbar.js
@@ -8,7 +8,6 @@ var NavbarTemplate = '<header class="am-topbar am-topbar-inverse">\n' +
     '\n' +
     '    <div class="am-collapse am-topbar-collapse" id="doc-topbar-collapse">\n' +
     '        <ul class="am-nav am-nav-pills am-topbar-nav">\n' +
-    '           <li id="NavbarIndexItemContainer"><a href="index.html">首页</li>\n' +
     '        </ul>\n' +
     '\n' +
     '        <form class="am-topbar-form am-topbar-left am-form-inline am-hide" role="search">\n' +
@@ -30,6 +29,61 @@ var NavbarTemplate = '<header class="am-topbar am-topbar-inverse">\n' +
     '    </div>\n' +
     '</header>';
 
+// 初始化其他菜单项
+function NavbarItemsRender() {
+    var items = [
+        {
+            name: "首页",
+            id: "0",
+            url: "index.html?column_id=0&page_size=10&page_num=1"
+        },
+        {
+            name: "资讯",
+            id: "1",
+            url: "list.html?column_id=1&page_size=10&page_num=1"
+        },
+        {
+            name: "贴吧",
+            id: "2",
+            url: "list.html?column_id=2&page_size=10&page_num=1"
+        },
+        {
+            name: "租房",
+            id: "3",
+            url: "list.html?column_id=3&page_size=10&page_num=1"
+        },
+        {
+            name: "拼车",
+            id: "4",
+            url: "carpooling_list.html?column_id=4&page_size=10&page_num=1"
+        },
+        {
+            name: "公众号",
+            id: "5",
+            url: "link.html?column_id=5&page_size=10&page_num=1"
+        }
+    ];
+    $.each(items, function (index, item) {
+        var a = $("<a></a>").attr("href", item.url).text(item.name);
+        var li = $("<li></li>");
+        if (item.id === NavbarPageEnv.column_id) {
+            $(document).attr("title", WebSiteTitle + item.name);
+            li.addClass("am-active");
+        }
+        $('.am-nav').append(li.append(a))
+    })
+}
+
+// 页面变量信息
+var NavbarPageEnv = {
+    creater_uid: createrUidDefault,
+    column_id: columnIdDefault,
+    order_by: orderByDefault,
+    page_size: PageSizeDefault,
+    page_num: PageStartNumberDefault,
+    is_end: false
+};
+
 // 初始化登录状态
 function NavbarSetSignStatus() {
     if (!Cookies.get('Authorization')) {
@@ -49,43 +103,22 @@ function UserLogoutHandler() {
     return false;
 }
 
+
 // 菜单栏初始化项及状态
 function NavbarInit() {
     $('body').prepend($(NavbarTemplate));
+    NavbarItemsRender();
+}
 
-    var req = {creater_uid: createrUidDefault};
-    APIColumnGetList(req, AlertShowAjaxError, function (data) {
-        if (data["code"] === 0) {
-            var column_id = GetURIParamIdValue(location.href, "column_id");
-            if (column_id === "" || column_id === columnIdDefault) {
-                if ((location.href.indexOf("index.html") !== -1) || (location.href.indexOf(".html") === -1)) {
-                    $("#NavbarIndexItemContainer").addClass("am-active");
-                    $(document).attr("title", WebSiteTitle + "首页");
-                }
-            }
-            if (!data["list"] || data["list"].length === 0) {
-                return;
-            }
-            $.each(data['list'], function (index, item) {
-                var url = "list.html?column_id=" + item.id + "&page_size=10&page_num=1";
-                if (item.name === "拼车") {
-                    url = "carpooling_list.html?column_id=" + item.id +  "&page_size=10&page_num=1";
-                }
-                var a = $("<a></a>").attr("href", url).text(item.name);
-                var li = $("<li></li>");
-                if (item.id === column_id) {
-                    $(document).attr("title", WebSiteTitle + item.name);
-                    li.addClass("am-active");
-                }
-                $('.am-nav').append(li.append(a))
-            });
-        } else {
-            AlertShowError(data['sub_error']);
-        }
-    });
+function NavbarGetCurrentEnv(currentUrl) {
+    NavbarPageEnv.column_id = GetURIParamIdValue(currentUrl, "column_id") || columnIdDefault;
+    NavbarPageEnv.order_by = GetURIParamStr(currentUrl, "order_by") || orderByDefault;
+    NavbarPageEnv.page_size = GetURIParamInt(currentUrl, "page_size") || PageSizeDefault;
+    NavbarPageEnv.page_num = GetURIParamInt(currentUrl, "page_num") || PageStartNumberDefault;
 }
 
 function NavbarRender() {
+    NavbarGetCurrentEnv(location.href);
     NavbarInit();
     NavbarSetSignStatus();
 }
